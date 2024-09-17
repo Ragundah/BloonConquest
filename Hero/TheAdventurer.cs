0939abbc4fadffaf2a8cebaf31c7378468864c51
Ragundah <ragundah@gmail.com>
2024-09-16 21:27:48 -0700

    Hero Initial Commit

---
+++ Hero/TheAdventurer.cs
@@ -0,0 +1,40 @@
+﻿using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+using Il2CppAssets.Scripts.Models.Towers;
+using Il2CppAssets.Scripts.Unity.Achievements.List;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BloonConquest.Hero
+{
+    internal class TheAdventurer : ModHero
+    {
+        
+
+        public override string BaseTower => TowerType.DartMonkey;
+        public override int Cost => 0;
+        public override string DisplayName => "ERROR FTGU";
+        public override string Title => "The Adventurer";
+        public override string Level1Description => "A Newbie Adventurer. Gains no special benefits.";
+        public override string Description => "Use 'Gear' and 'Talents' to gain powerful meta upgrades!";
+        public override string NameStyle => TowerType.StrikerJones;
+        public override string BackgroundStyle => TowerType.Geraldo;
+        public override string GlowStyle => TowerType.AdmiralBrickell;
+        public override int MaxLevel => 1;
+        public override float XpRatio => 1;
+
+        [Obsolete]
+        public override int Abilities => 0;
+        public override void ModifyBaseTowerModel(TowerModel towerModel)
+        {
+            towerModel.range = 10;
+            var attackModel = towerModel.GetAttackModel();
+            attackModel.range = 10;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,10 +3,11 @@
     <img align="left" alt="Icon" height="90" src="Icon.png">
     <img align="right" alt="Download" height="75" src="https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/BloonsTD6%20Mod%20Helper/Resources/DownloadBtn.png">
 </a>
-BloonConquest
+Bloon Conquest
 </h1>
 
 Bloons Conquest, a Standalone RPG Mod!
+
 More info coming soon :)
 
 [![Requires BTD6 Mod Helper](https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/banner.png)](https://github.com/gurrenm3/BTD-Mod-Helper#readme)
