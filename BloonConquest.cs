e7eee61d57a9a1c1114d1b75e46e7ee1781a601b
Ragundah <83891181+Ragundah@users.noreply.github.com>
2024-09-16 18:23:58 -0700

    Initial Upload

---
+++ BloonConquest.cs
@@ -0,0 +1,36 @@
+using MelonLoader;
+using BTD_Mod_Helper;
+using BloonConquest;
+using BTD_Mod_Helper.Extensions;
+
+[assembly: MelonInfo(typeof(BloonConquest.BloonConquest), ModHelperData.Name, ModHelperData.Version, ModHelperData.Author)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace BloonConquest;
+
+public class BloonConquest : BloonsTD6Mod
+{
+    public static string ModTitle = "Bloon Conquest RPG";
+    public static string ModInitials = "BC-RPG";
+    public static string PlayerName = "";
+
+    public static BloonConquest instance;
+
+    public override void OnApplicationStart()
+    {
+        instance = this;
+        ModHelper.Msg<BloonConquest>(ModTitle + " Loaded");
+        //ModHelper.Msg<BloonConquest>(ModInitials + " Found Username: "+ PlayerName);
+    }
+
+    public void SetName(string name)
+    {
+        PlayerName = name;
+        ModHelper.Msg<BloonConquest>(ModInitials + " Found Username: " + PlayerName);
+    }
+    public override void OnMainMenu()
+    {
+        SetName(BTD_Mod_Helper.Api.Helpers.Instances.Game.GetPlayerLiNKAccount().DisplayName);
+        base.OnMainMenu();
+    }
+}
